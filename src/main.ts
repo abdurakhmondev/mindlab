24357bdbe8339a8c0cd91e7054eeb5e68833cc5f
tmonbek <abdurakhmon@iota.uz>
2026-03-07 02:41:26 +0500

    first commit

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: true,
+    }),
+  );
+
+  app.enableCors();
+
+  // Global prefix
+  app.setGlobalPrefix('api/v1');
+
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+}
+
+bootstrap();
